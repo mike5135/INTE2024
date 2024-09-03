3f52f541d673e19621a2dc9ce55712999da1d166
make <143557535+mike5135@users.noreply.github.com>
2024-09-03 23:46:44 +0200

    Add files via upload

---
+++ Counter_2/src/main/java/Counter_2.java
@@ -0,0 +1,15 @@
+public class Counter_2 {
+
+    private  int  value;
+
+    public void inc() {
+        value ++;
+    }
+
+    public int getValue() {
+
+        return value;
+    }
+// test 2, 3
+
+}
