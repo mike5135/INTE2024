61e360c8ebbe87877449055c06d98526d258a0bd
mike5135 <micpap@kth.se>
2024-09-04 18:07:26 +0200

    test33

--- Counter/src/main/java/inte/Counter.java
+++ Counter/src/main/java/inte/Counter.java
@@ -1,4 +1,44 @@
 package inte;
 
 public class Counter {
+
+
+    private int value;
+
+
+    public void inc() {
+        value+=2;
+    }
+
+    public void dec() {
+        value-=2;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String toString() {
+
+
+        int p = 3;
+        String s = "Hi";
+
+        return "This is the current value 1 + 23"  + p + s + value;
+
+    }
+
+    // test push, one more
+    public int m1() {
+        return 1+1;
+    }
+
+    /*
+    public static void main(String[] args) {
+
+
+        System.out.println("Mikael4");
+    }
+    */
 }
+
--- Counter/src/test/java/inte/CounterTest.java
+++ Counter/src/test/java/inte/CounterTest.java
@@ -1,7 +1,44 @@
 package inte;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-class CounterTest {
+public class CounterTest {
+    static Counter createCounterWithValueOne() {
+        Counter counter = new Counter();
+        counter.inc();
+        return counter;
+    }
+    @Test
+    void newCounterInitializedToZero() {
+        Counter c = new Counter();
+        assertEquals(0, c.getValue());
+    }
 
+    @Test
+    void IncIncreasesValue() {
+        Counter c = new Counter();
+        c.inc();
+        assertEquals(2, c.getValue());
+        //Dorra
+    }
+
+    @Test
+    void DecDecreasesValue() {
+        Counter c = createCounterWithValueOne();
+        c.dec();
+        assertEquals(0, c.getValue());
+    }
+
+    @Test
+    void toStringReturnsCorrectString() {
+        Counter c = createCounterWithValueOne();
+        assertEquals("This is the current value 1", c.toString());
+    }
+
+    @Test
+    void testm1() {
+        Counter c = new Counter();
+        assertEquals(2, c.m1());
+    }
 }
